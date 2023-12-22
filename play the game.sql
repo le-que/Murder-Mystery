aa6fedd98f20143b20dbe131274009ec1f405e2d
le-que <128601594+le-que@users.noreply.github.com>
2023-12-22 12:43:56 -0500

    Delete play the game.sql

--- play the game.sql
+++
@@ -1,6 +0,0 @@
-call `the detective club murder`.introduceSuspects();
-call `the detective club murder`.getQuestions();
--- Choose some people to answer your questions (Enter the id)
-call `the detective club murder`.getAnswer();
--- Choose the person you think is the murder and hear their confession (Enter their name, Ex. shown below)
-call `the detective club murder`.getConfession('Alex');
